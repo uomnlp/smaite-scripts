dd495fa3ca1daf039c20d0da712fde0f260e9e72
schlevik <viktor_schlegel@asus.com>
2023-02-13 10:15:44 +0800

    add app saving

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from flask import Flask, request
@@ -8,21 +9,34 @@ app = Flask(__name__)
 CORS(app)
 
 file_name = 'workers.txt'
+auth_key = 'Asdf1234'
 
 
-@app.route('/')
-def get_workers():
-    try:
-     with open(file_name, 'r') as f:
-        worker = request.args.get("worker_id")
-        for line in f.readlines():
-            if worker.lower() in line.lower():
-                return "true"
-        return "false"
-    except:
-        return "false"
 
 if __name__ == '__main__':
+    with open(file_name, 'r') as f:
+        workers = set(l for l in f.read().splitlines() if l)
+    
+    @app.route('/')
+    def get_workers():
+        worker = request.args.get("worker_id", None)
+        if worker:
+            return json.dumps(worker in workers)
+        worker = request.args.get("save_worker_id", None)
+        key = request.args.get("key", None)
+        if worker and key:
+            if key != auth_key:
+                print("Wrong auth key")
+                return "false"
+            else:
+                print(f"Adding {worker}!")
+                workers.add(worker)
+                return "true"
+        return "false"
+
+    
+
+
     port = int(os.environ.get('PORT', 5000))
 #    app.run(host='0.0.0.0', port=port)
     server = WSGIServer(('', port), app)
@@ -31,3 +45,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Shutting down...")
         server.close()
+        print("Saving workers")
+        with open(file_name, 'w+') as f:
+            f.write('\n'.join(workers) + '\n')
